cf0b1f47a1049d8930bf76224554a82c6f72700e
Axel Moinet <axel.moinet@codium.fr>
2020-11-03 14:43:28 +0100

    Corrected autostart.sh script

--- autostart.sh
+++ autostart.sh
@@ -3,20 +3,32 @@
 # X11 related tools
 nitrogen --restore
 
-[ -n "ps -C picom" ] || \
-  picom --config ~/.local/share/dwm/picom.conf
+[ -n "$(ps -C picom --no-headers)" ] || \
+  picom --config ~/.local/share/dwm/picom.conf &
 
 # System utilities / applets
-[ -n "pgrep polkit-gnome" ] || \
+[ -n "$(pgrep dunst)" ] || \
+  dunst -conf ~/.config/dunst/dunstrc &
+
+[ -n "$(pgrep polkit-gnome)" ] || \
   /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
-[ -n "ps -C udiskie" ] || \
+[ -n "$(ps -C udiskie --no-headers)" ] || \
   udiskie -c ~/.config/udiskie/config.yml &
 
-[ -n "ps -C blueman-applet" ] || \
+[ -n "$(ps -C blueman-applet --no-headers)" ] || \
   blueman-applet &
 
-[ -n "ps -C nextcloud" ] || \
-  nextcloud --background
+[ -n "$(ps -C nextcloud --no-headers)" ] || \
+  nextcloud --background &
 
-xscreensaver -no-splash &
+[ -n "$(ps -C xscreensaver --no-headers)" ] || \
+  xscreensaver -no-splash &
+
+[ -n "$(pgrep gnome-keyring)" ] || \
+  gnome-keyring-daemon --start --components=secrets
+
+[ -n "$(command -v displayset)" ] && displayset
+
+[ -n "$(command -v numlockx)" ] && numlockx on
+
